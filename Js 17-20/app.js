c562322f6ebc300f1948849043b8aa427db09eaa
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-02-16 20:51:14 +0500

    added files

---
+++ Js 17-20/app.js
@@ -0,0 +1,181 @@
+// >>>>>>>>>>CHAPTER 17 to 20
+
+//Question no 01
+
+// var arr = [[],[]];
+// console.log(arr);
+
+//Question no 02
+
+// var matrix = [["0 1 2 3"], ["1 0 1 2"], ["2 1 0 1"]];
+// for(i=0; i<3; i++){
+//     document.write(matrix[i] + "<br/>");
+// }
+
+
+//Question no 3
+
+// var i;
+// for(var i=1; i<=10; i++) {
+//     console.log(i);
+// }
+
+
+//Question no 04
+
+
+// var num = prompt("Enter a number to show multiplication.")
+
+// var length = prompt("enter the length");
+// for (var i=1; i<=length; i++) {
+//     document.write(num + "x" + i + "=" + num*i +"<br/>");
+// }
+
+
+//Question no 05
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for(i=0; i<fruits.length; i++) {
+//     document.write(fruits[i] + "<br/>");
+// }
+// document.write("Element at index 0 is " + fruits[0] + "<br/>");
+// document.write("Element at index 1 is " + fruits[1] + "<br/>");
+// document.write("Element at index 2 is " + fruits[2] + "<br/>");
+// document.write("Element at index 3 is " + fruits[3] + "<br/>");
+// document.write("Element at index 4 is " + fruits[4]);
+
+
+//Question no 06
+//(a)
+// for (var i=0; i<=15; i++) {
+    // console.log(i);
+    //document.write("<h2>Counting:</h2>");
+    // document.write(i + ",")
+// }
+
+//(b)
+// for (var i=10; i>=0; i--) {
+//     console.log(i);
+//document.write("<h2>Reverse Counting:</h2>");
+// document.write(i + ",")
+// }
+
+//(c)
+// for (var i=0; i<=20; i+=2) {
+    //document.write("<h2>Even:</h2>");
+    // document.write(i + ",");
+// }
+
+//(d)
+// for (var i=1; i<20; i+=2) {
+    //document.write("<h2>Odd:</h2>");
+//     document.write(i + ",");
+// }
+
+//(e)
+// for (var i=2; i<=20; i+=2) {
+    //document.write("<h2>Series:</h2>");
+//     document.write(i + "k,");
+// }
+
+//Question no 07
+
+// var items = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var search = prompt("Enter which you want to search?");
+// flag = "false";
+// for(i=0; i<5; i++){
+// if(search == items[i]) {
+//     alert("Yes Available.");
+//     break;
+// }
+// else{ 
+//     alert("Not Available.");
+//     break;
+// }
+// }
+
+
+//Question no 08
+// var arr = [24,53,78,91,12];
+// function maximum(){
+//     var arr = [24,53,78,91,12];
+//     var max = arr[0]
+//     for(let i =0; i<arr.length; i++){
+//         if (arr[i]>max){
+//             max = arr[i]
+//         }
+//     } 
+//     document.write("The maximum value is " + max); 
+// }
+// maximum();
+
+//Question no 09
+
+// var arr = [24,53,78,91,12];
+// function minimum(){
+//     var arr = [24,53,78,91,12];
+//     var min = arr[0]
+//     for(let i =0; i<arr.length; i++){
+//         if (arr[i]<min){
+//             min = arr[i]
+//         }
+//     } 
+//     document.write("The minimum value is " + min); 
+// }
+// minimum();
+
+
+
+
+
+
+
+
+
+
+
+//Question no 10
+// var x; for(x=5; x<=100; x+=5){
+//     console.log(x);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
